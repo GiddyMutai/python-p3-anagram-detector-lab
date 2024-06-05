1c3108d6d108e5758d43245da5ff2aec63d7e005
Gideon Mutai <giddymutai13@gmail.com>
2024-06-05 20:50:10 +0300

    Finished task

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,15 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+        self.result = []
+        self.characters = []
+     
+    def match(self, word_list):
+        self.characters = [letter for letter in self.word]
+        for element in word_list:
+            element_chars = [letter for letter in element]
+            if sorted(element_chars) == sorted(self.characters):
+                self.result.append(element)
+            else:
+                continue
+        return self.result
